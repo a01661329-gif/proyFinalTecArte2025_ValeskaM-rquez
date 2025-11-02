61b8ba20854aa2563f52d07c365c166aa02122de
Daniela Valeska Márquez Rosas <danielavaleska@Daniels-MacBook-Air.local>
2025-11-02 01:51:44 -0600

    Comentarios añadidos al código

--- figurasPrincipal.py
+++ figurasPrincipal.py
@@ -1,20 +1,20 @@
 
 import pandas as pd
-from funciones import triangulo, rectangulo, circulo
+from funciones import triangulo, rectangulo, circulo	#Llama las funciones definidas en fnciones.py
 
-dataFile = pd.read_csv("figuras.csv")
+dataFile = pd.read_csv("figuras.csv")		#Lee el archivo que contiene las medidas de las figuras
 
 print("Procesando figuras ...\n")
 
 areas = []
 perimetros = []
 
-for index, row in dataFile.iterrows():
-	if row['FIGURA'] == 't':
+for index, row in dataFile.iterrows():		#itera por cada fila
+	if row['FIGURA'] == 't':		#decide qué fórmula usar según la figura
 		area = triangulo(row['MEDIDA1'], row ['MEDIDA2'])
 	elif row['FIGURA'] == 'r':
 		area = rectangulo(row['MEDIDA1'], row ['MEDIDA2'])
 	elif row['FIGURA'] == 'c':
 		area = circulo(row['MEDIDA1'])
 	print(f"Fila {index}: FIGURA={row['FIGURA']}, Medida1={row['MEDIDA1']}, Medida2={row['MEDIDA2']}")
-	print(f"Fila {index}: Area:{area}")
+	print(f"Fila {index}: Area:{area}")		#imprime el resultado
--- funciones.py
+++ funciones.py
@@ -1,11 +1,11 @@
 import math
 
-def circulo(MEDIDA1):
+def circulo(MEDIDA1):				#Define la funcion del aea del circulo
 	return math.pi * MEDIDA1**2
 
-def rectangulo(MEDIDA1, MEDIDA2):
+def rectangulo(MEDIDA1, MEDIDA2):		#Define la funcion del area del rectangulo
 	return MEDIDA1 * MEDIDA2
 
-def triangulo(MEDIDA1, MEDIDA2):
+def triangulo(MEDIDA1, MEDIDA2):		#DEfine la funcion del area del triangulo
 	return (MEDIDA1 * MEDIDA2) / 2
 
