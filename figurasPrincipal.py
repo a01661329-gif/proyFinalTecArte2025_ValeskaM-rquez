7330b41b9525bba98877dd581004bd4d40043243
Daniela Valeska Márquez Rosas <danielavaleska@Daniels-MacBook-Air.local>
2025-11-02 01:43:26 -0600

    Calcula áreas

--- figurasPrincipal.py
+++ figurasPrincipal.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+from funciones import triangulo, rectangulo, circulo
 
 dataFile = pd.read_csv("figuras.csv")
 
@@ -9,4 +10,11 @@ areas = []
 perimetros = []
 
 for index, row in dataFile.iterrows():
+	if row['FIGURA'] == 't':
+		area = triangulo(row['MEDIDA1'], row ['MEDIDA2'])
+	elif row['FIGURA'] == 'r':
+		area = rectangulo(row['MEDIDA1'], row ['MEDIDA2'])
+	elif row['FIGURA'] == 'c':
+		area = circulo(row['MEDIDA1'])
 	print(f"Fila {index}: FIGURA={row['FIGURA']}, Medida1={row['MEDIDA1']}, Medida2={row['MEDIDA2']}")
+	print(f"Fila {index}: Area:{area}")
---
+++ funciones.py
@@ -0,0 +1,11 @@
+import math
+
+def circulo(MEDIDA1):
+	return math.pi * MEDIDA1**2
+
+def rectangulo(MEDIDA1, MEDIDA2):
+	return MEDIDA1 * MEDIDA2
+
+def triangulo(MEDIDA1, MEDIDA2):
+	return (MEDIDA1 * MEDIDA2) / 2
+
